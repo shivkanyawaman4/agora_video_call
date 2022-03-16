945b9ec468a1ae15cf31f5db94e9978348400e36
shivkanyawaman4 <wamanshivkanya@gmail.com>
2022-03-16 23:42:42 +0530

    :blossom:  start with implementation of video call, one side video call with fix token working fine

--- lib/main.dart
+++ lib/main.dart
@@ -81,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Agora Flutter SDK'),
+        title: const Text('Agora Video Call'),
       ),
       body: Stack(
         children: [
